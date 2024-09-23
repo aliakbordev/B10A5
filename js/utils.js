b095903a2864e6d57f5fcb06bcf2e3321c55044d
mdaliakbor <aliakbor.js@gmail.com>
2024-09-23 17:43:15 +0600

    history date make dynamic

--- js/utils.js
+++ js/utils.js
@@ -11,8 +11,7 @@ document
     const balanceField = parentElement.querySelector("#balance");
     let balance = parseFloat(balanceField.innerText);
 
-    const title = parentElement.querySelector("#title").innerText;
-    console.log(title)
+    const heading = parentElement.querySelector("#title").innerText;
 
     const amountField = parentElement.querySelector("#amount");
     const amount = parseFloat(amountField.value);
@@ -21,26 +20,29 @@ document
       alert("Please enter a valid amount.");
       return;
     }
+
+    
     alert("done");
     mainBalanceField.innerText = mainBalance - amount;
     balanceField.innerText = balance + amount;
     amountField.value = "";
 
-const item = document.createElement("div"); // Create a div element
 
-// Add classes and content to the div using innerHTML
-item.innerHTML = `
+
+const history = document.createElement("div");
+
+history.innerHTML = `
     <div class="p-8 border rounded-lg mt-8">
-        <h4 class="text-lg font-bold">96500 Taka is Donated for famine-2024 at Feni, Bangladesh</h4>
-        <p class="text-base text-gray-700 pt-2">Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh Standard Time)</p>
+        <h4 class="text-lg font-bold">${amount} Taka is ${heading}</h4>
+        <p class="text-base text-gray-700 pt-2">Date : ${
+          new Date().toString()
+        }</p>
     </div>
 `;
 
-// Append the created element to the history section
-document.getElementById("history_section").appendChild(item);
 
-// Log the created item
-console.log(item);
+document.getElementById("history_section").appendChild(history);
+
 
   });
 
